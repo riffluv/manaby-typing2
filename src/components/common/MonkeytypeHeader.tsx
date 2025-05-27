0bff7407bc955155a2a021f7c26d8b8d336cf0ac
riffluv <tomatotabetai69@gmail.com>
2025-05-27 17:38:06 +0900

    cssReset

--- src/components/common/MonkeytypeHeader.tsx
+++ src/components/common/MonkeytypeHeader.tsx
@@ -12,21 +12,21 @@ const icons = [FiMonitor, FiBarChart2, FiInfo, FiSettings, FiUser];
 
 const MonkeytypeHeader = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.logoContainer}>
+    <header className={`${styles.header} preserved-header`}>
+      <div className={`${styles.logoContainer} preserved-logo-container`}>
         <Image
           src="/images/manabyicon_01.png"
           alt="manabytype logo"
           width={34}
           height={34}
-          className={styles.logoImage}
+          className={`${styles.logoImage} preserved-logo-image`}
           priority
-        />        <div className={styles.logoTextContainer}>
-          <span className={styles.logoText}>manabytype</span>
+        />        <div className={`${styles.logoTextContainer} preserved-logo-text-container`}>
+          <span className={`${styles.logoText} preserved-logo-text`}>manabytype</span>
         </div>
-      </div>      <div className={styles.iconGroup}>        {icons.map((Icon, i) => (
-          <span key={i} className={styles.icon}>
-            <Icon size={22} className={styles.iconSvg} />
+      </div>      <div className={`${styles.iconGroup} preserved-icon-group`}>        {icons.map((Icon, i) => (
+          <span key={i} className={`${styles.icon} preserved-icon`}>
+            <Icon size={22} className={`${styles.iconSvg} preserved-icon-svg`} />
           </span>
         ))}
       </div>
