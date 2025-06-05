9e96e5b73db9701fcd92989a370a9cc1d0a67930
riffluv <tomatotabetai69@gmail.com>
2025-06-05 18:03:04 +0900

    typingfairutukattenaiyatusakujo

--- typingmania-ref/src/lib/fontloader.js
+++
@@ -1,9 +0,0 @@
-export default class FontLoader {
-  static async load (url, family, weight, style) {
-    const font = new FontFace(family, `url(${url})`, {
-      family, style, weight,
-    })
-    await font.load()
-    document.fonts.add(font)
-  }
-}
